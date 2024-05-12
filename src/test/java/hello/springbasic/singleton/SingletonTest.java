47dc14cbeb8ce914d32c963f0280ea65a884dd31
coolcool <coolcool.with.you@gmail.com>
2024-05-12 21:55:30 +0900

    ADD - 싱글톤 방식의 주의점(stateless)

--- src/test/java/hello/springbasic/singleton/SingletonTest.java
+++ src/test/java/hello/springbasic/singleton/SingletonTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 public class SingletonTest {
     @Test
@@ -59,5 +60,52 @@ public class SingletonTest {
         System.out.println("memberService1 = " + memberService1);
         System.out.println("memberService2 = " + memberService2);
         assertThat(memberService1).isSameAs(memberService2);
+    }
+
+    @Test
+    @DisplayName("상태를 유지하는 스프링 빈을 설계하면 안 된다.")
+    void statefulServiceSingleton() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+        StatefulService statefulService1 = ac.getBean("statefulService", StatefulService.class);
+        StatefulService statefulService2 = ac.getBean("statefulService", StatefulService.class);
+
+        // ThreadA: A사용자 10000원 주문
+        statefulService1.order("userA", 10000);
+
+        // ThreadB: B사용자 20000원 주문
+        statefulService2.order("userB", 20000);
+
+        // ThreadA: 사용자A 주문 금액 조회
+        int price = statefulService1.getPrice();
+
+        // ThreadA: 사용자A는 10000원을 기대했지만, 기대와 다르게 20000원 출력
+        System.out.println("price = " + price);
+        assertThat(statefulService1.getPrice()).isEqualTo(20000);
+    }
+
+    @Test
+    @DisplayName("스프링 빈은 무상태여야 한다.")
+    void statelessServiceSingleton() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+        StatefulService statefulService1 = ac.getBean("statefulService", StatefulService.class);
+        StatefulService statefulService2 = ac.getBean("statefulService", StatefulService.class);
+
+        // ThreadA: A사용자 10000원 주문
+        int userAOrderPrice = statefulService1.orderStateless("userA", 10000);
+
+        // ThreadB: B사용자 20000원 주문
+        int userBOrderPrice = statefulService2.orderStateless("userB", 20000);
+
+        System.out.println("userA price = " + userAOrderPrice);
+        System.out.println("userB price = " + userBOrderPrice);
+        assertThat(userAOrderPrice).isEqualTo(10000);
+        assertThat(userBOrderPrice).isEqualTo(20000);
+    }
+
+    static class TestConfig {
+        @Bean
+        public StatefulService statefulService() {
+            return new StatefulService();
+        }
     }
 }
