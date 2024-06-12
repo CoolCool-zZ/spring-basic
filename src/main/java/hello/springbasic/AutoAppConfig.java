59a7766cf1e7b53478f75a1a0aeab8c1b5fc3ae2
coolcool <coolcool.with.you@gmail.com>
2024-06-12 09:50:25 +0900

    ADD - 중복 등록과 충돌

--- src/main/java/hello/springbasic/AutoAppConfig.java
+++ src/main/java/hello/springbasic/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.springbasic;
 
+import hello.springbasic.member.MemberRepository;
+import hello.springbasic.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -8,4 +11,9 @@ import org.springframework.context.annotation.FilterType;
 @ComponentScan(
         excludeFilters =
                 @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class))
-public class AutoAppConfig {}
+public class AutoAppConfig {
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+}
--- src/test/java/hello/springbasic/scan/AutoAppConfigTest.java
+++ src/test/java/hello/springbasic/scan/AutoAppConfigTest.java
@@ -2,12 +2,23 @@ package hello.springbasic.scan;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
 import hello.springbasic.AutoAppConfig;
 import hello.springbasic.member.MemberService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class AutoAppConfigTest {
+
+    @BeforeEach
+    void changeLogLevel() {
+        final Logger logger = (Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
+        logger.setLevel(Level.DEBUG);
+    }
+
     @Test
     void basicScan() {
         AnnotationConfigApplicationContext ac =
